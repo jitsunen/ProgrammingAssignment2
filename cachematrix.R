82b492cdd41982312d6700881cf05b126a51b042
Manu Yadav <manuyadav.ubc@gmail.com>
2015-03-06 15:18:01 -0800

    Fixed some comments

--- cachematrix.R
+++ cachematrix.R
@@ -3,10 +3,9 @@
 # Code created:  6th Mar 2015                                                   
 #
 # This script contains two functions to help compute and cache the inverse of a matrix.
-# the function makeCacheMatrix returns a list of functions which provide caching ability to a 
-# passed in matrix. The function cacheSolve utilizes the matrix created through makeCacheMatrix
-# to check the cache before computing its inverse.
-
+# The function makeCacheMatrix returns a list of functions which provide caching ability to a 
+# passed in matrix. The function cacheSolve accepts a matrix created through makeCacheMatrix function
+# and returns its inverse either from a cache or computes the inverse if it was not found in the cache.
 
 makeCacheMatrix <- function(x = matrix()) {
   # Construct and return a list of functions to cache and return the inverse of a passed in matrix.
@@ -29,13 +28,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 cacheSolve <- function(x, ...) {
-  # Computes the inverse of a passed in matrix, created through the makeCacheMatrix, and returns it. 
-  # If the inverse has already been computed by this function then it simply returns the computed 
-  # inverse from its cache.
+  # Computes the inverse of a passed in matrix, which was created through the makeCacheMatrix function, 
+  # and returns it. If the inverse was already computed by this function on a previous invocation then it 
+  # simply returns the computed inverse from the cache.
   #
   # Args:
   #  x: matrix whose inverse is to be made cacheable.
-  #  ...: arguments passed to solve to compute inverse of x.
+  #  ...: arguments passed to solve() to compute inverse of x.
   #  
   # Returns: 
   #  inverse of matrix x
